5fdcaa72b3f685a8d098045ee0f031c02df3d361
azxelbrenes <134638844+azxelbrenes@users.noreply.github.com>
2026-06-19 19:56:37 -0600

    Automapper

--- Parchis_G3/Parchis_G3.Dominio/DTO/AutoMapperProfile.cs
+++ Parchis_G3/Parchis_G3.Dominio/DTO/AutoMapperProfile.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using Parchis_G3.Dominio.Entidades;
-using Parchis_G3.Dominio.DTO;
+using Parchis_G3.Dominio.EntidadesTipadas;
 
 namespace Parchis_G3.Dominio.DTO
 {
@@ -8,7 +8,50 @@ namespace Parchis_G3.Dominio.DTO
     {
         public AutoMapperProfile()
         {
+            // Sala
+            CreateMap<Sala, TSala>().ReverseMap();
 
+            // TiposArticulo
+            CreateMap<TiposArticulo, TTiposArticulo>().ReverseMap();
+
+            // Usuario
+            CreateMap<Usuario, TUsuario>().ReverseMap();
+
+            // Articulo
+            CreateMap<Articulo, TArticulo>().ReverseMap();
+
+            // UsuarioArticulo
+            CreateMap<UsuarioArticulo, TUsuarioArticulo>().ReverseMap();
+
+            // EquipamientoActivo
+            CreateMap<EquipamientoActivo, TEquipamientoActivo>().ReverseMap();
+
+            // Partida
+            CreateMap<Partida, TPartida>().ReverseMap();
+
+            // JugadoresPartidum
+            CreateMap<JugadoresPartidum, TJugadoresPartidum>().ReverseMap();
+
+            // EstadoFicha
+            CreateMap<EstadoFicha, TEstadoFicha>().ReverseMap();
+
+            // TurnosPartidum
+            CreateMap<TurnosPartidum, TTurnosPartidum>().ReverseMap();
+
+            // Transaccione
+            CreateMap<Transaccione, TTransaccione>().ReverseMap();
+
+            // HistorialPartida
+            CreateMap<HistorialPartida, THistorialPartida>().ReverseMap();
+
+            // MensajesChat
+            CreateMap<MensajesChat, TMensajesChat>().ReverseMap();
+
+            // FilaEspera
+            CreateMap<FilaEspera, TFilaEspera>().ReverseMap();
+
+            // SesionesActiva
+            CreateMap<SesionesActiva, TSesionesActiva>().ReverseMap();
         }
     }
 }
