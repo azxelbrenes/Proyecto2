38f0fab76031ac53a0ed3bb33f012fc502376f38
azxelbrenes <134638844+azxelbrenes@users.noreply.github.com>
2026-07-15 18:10:08 -0600

    diseño del login hecho pero aun falta hacer funcionar botones de crear cuenta y el login

--- Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
+++ Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
@@ -1,17 +1,22 @@
+
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
+    // Ruta vacía → redirige al login automáticamente
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () =>
+      import('./pages/login/login.page').then(m => m.LoginPage)
   },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.page').then(m => m.HomePage)
+  }
 ];
