c112ad4c9aa4efdaa3f9777f274964f82fefa6b4
azxelbrenes <134638844+azxelbrenes@users.noreply.github.com>
2026-07-30 14:54:43 -0600

    nuevo app routes

--- Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
+++ Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 
-
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    // Ruta vacía → redirige al login automáticamente
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
@@ -15,16 +13,13 @@ export const routes: Routes = [
       import('./pages/login/login.page').then(m => m.LoginPage)
   },
   {
+    path: 'registro',
+    loadComponent: () =>
+      import('./pages/registro/registro.page').then(m => m.RegistroPage)
+  },
+  {
     path: 'home',
     loadComponent: () =>
-      import('./home/home.page').then(m => m.HomePage)
-  },
-  {
-    path: 'registro',
-    loadComponent: () => import('./pages/registro/registro.page').then( m => m.RegistroPage)
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+      import('./pages/home/home.page').then(m => m.HomePage)
   }
 ];
