c2f9d7f018ed985a4f068cc8ddc357b28c33e60a
azxelbrenes <134638844+azxelbrenes@users.noreply.github.com>
2026-07-30 09:05:56 -0600

    pagina home

--- Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
+++ Parchis_G3/ParchisOnline_FrontEnd/ParchisOnline_FrontEnd/src/app/app.routes.ts
@@ -22,5 +22,9 @@ export const routes: Routes = [
   {
     path: 'registro',
     loadComponent: () => import('./pages/registro/registro.page').then( m => m.RegistroPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   }
 ];
